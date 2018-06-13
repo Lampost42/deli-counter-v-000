4b3f40a141e3784af63c10a2c695bdc83d87b897
Trevor Russell <truss.42@gmail.com>
2018-06-13 17:05:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,7 @@ def line(customers)
     puts "The line is currently empty."
   else
     customers.collect!.with_index {|customer, i| "#{i + 1}. #{customer} "
+    customers.join
+    customers.chop!
+    
 end
